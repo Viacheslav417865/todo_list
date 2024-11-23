28357dc75f47bbe34de04e61843dba5d7489516a
Viacheslav417865 <sfinks417865@gmail.com>
2024-11-23 19:57:27 +0200

    Update forms_extras.py with custom template filters

--- todolist/tasks/templates/tasks/templatetags/forms_extras.py
+++ todolist/tasks/templates/tasks/templatetags/forms_extras.py
@@ -5,4 +5,4 @@ register = template.Library()
 
 @register.filter
 def add_class(value, class_name):
-    return value.as_widget(attrs={'class': class_name})
+    return value.as_widget(attrs={"class": class_name})
