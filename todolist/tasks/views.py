149523c72303376170b86380c64b4e981de0215b
Viacheslav417865 <sfinks417865@gmail.com>
2024-11-23 19:05:37 +0200

    Update views.py and add initial migration for tasks

--- todolist/tasks/views.py
+++ todolist/tasks/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task, Tag
+from .forms import TaskForm, TagForm
 
-# Create your views here.
+
+def home(request):
+    tasks = Task.objects.all()
+    return render(request, "tasks/home.html", {"tasks": tasks})
+
+def tag_list(request):
+    tags = Tag.objects.all()
+    return render(request, "tasks/tag_list.html", {"tags": tags})
+
+def create_task(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    else:
+        form = TaskForm()
+    return render(request, "tasks/create_task.html", {"form": form})
+
+def create_tag(request):
+    if request.method == "POST":
+        form = TagForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("tag_list")
+    else:
+        form = TagForm()
+    return render(request, "tasks/create_tag.html", {"form": form})
+
+def update_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    else:
+        form = TaskForm(instance=task)
+    return render(request, "tasks/create_task.html", {"form": form})
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    task.delete()
+    return redirect("home")
+
+def toggle_task_status(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    task.is_done = not task.is_done
+    task.save()
+    return redirect("home")
