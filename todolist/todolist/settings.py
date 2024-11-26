917417946b2d82db0507a7427d14b9e94c79fae5
Viacheslav417865 <sfinks417865@gmail.com>
2024-11-26 16:56:55 +0200

    Update settings.py and add requirements.txt and screenshots

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'rest_framework',
     'tasks',
     'crispy_forms',
     'crispy_bootstrap5',
